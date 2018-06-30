447030df49d9953ac4347001a7126e11fdb476ea
Esmail Shabayek <esmail.shabayek@gmail.com>
2018-06-30 03:14:01 +0200

    add servcie worker

--- js/app.js
+++ js/app.js
@@ -83,5 +83,17 @@ const getcurrencies = () => {
   }).catch(error => console.log(error));
 }
 
+if ('serviceWorker' in navigator) {
+  // Register a service worker hosted at the root of the
+  // site using a more restrictive scope.
+  navigator.serviceWorker.register('/sw.js', {scope: './'}).then(function(registration) {
+    console.log('Service worker registration succeeded:', registration);
+  }).catch(function(error) {
+    console.log('Service worker registration failed:', error);
+  });
+} else {
+  console.log('Service workers are not supported.');
+}
+
 
 getcurrencies();
---
+++ sw.js
@@ -0,0 +1,46 @@
+const staticCacheName = 'converter-static-v80';
+
+self.addEventListener('install', function(event) {
+
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+	    '/',
+		'/index.html',
+        '/style.css',
+        '/app.js',
+        'css/bootstrap.min.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('wittr-') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('message', function(event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
